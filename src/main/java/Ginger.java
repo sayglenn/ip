eaec09d298f3c753a62f1d491d821aae033ead58
Say Glenn <glennsay1234@gmail.com>
2024-08-21 18:27:25 +0800

    Removed Duke bot and added Ginger bot

---
+++ src/main/java/Ginger.java
@@ -0,0 +1,12 @@
+public class Ginger {
+    private final static String HORIZONTAL_LINE = "____________________________________________________________";
+    private final static String BOT_NAME = "Ginger";
+    public static void main(String[] args) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm " + BOT_NAME);
+        System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+}
