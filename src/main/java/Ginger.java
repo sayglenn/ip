a5fc6324cc8add03a7ec4ca98335927614d764c6
Say Glenn <glennsay1234@gmail.com>
2024-08-21 19:22:21 +0800

    Added scanner to take in user input

--- src/main/java/Ginger.java
+++ src/main/java/Ginger.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class Ginger {
     private final static String HORIZONTAL_LINE = "____________________________________________________________";
     private final static String BOT_NAME = "Ginger";
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Greet the user
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm " + BOT_NAME);
         System.out.println("What can I do for you?");
         System.out.println(HORIZONTAL_LINE);
+
+        String input = sc.nextLine();
+        System.out.println(HORIZONTAL_LINE + "\n" + input + "\n" + HORIZONTAL_LINE);
+        sc.close();
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(HORIZONTAL_LINE);
     }
