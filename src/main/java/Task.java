429c639f20abc72be9ff2a78b76c0b049d09b8a4
Say Glenn <glennsay1234@gmail.com>
2024-08-24 19:46:06 +0800

    Change Task.java to abstract and add Javadocs

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,15 @@
-public class Task {
+/**
+ * This abstract class encapsulates a Task which contains a title and completion.
+ * @author Say Glenn
+ */
+public abstract class Task {
     private final String title;
     private boolean isCompleted;
 
+    /**
+     * A constructor for a Task.
+     * @param title A string which is the title of the task.
+     */
     public Task(String title) {
         this.title = title;
         this.isCompleted = false;
