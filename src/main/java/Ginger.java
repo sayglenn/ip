ac913cfe8ee92bb2393c6a7e835bec6d1bc8b559
Say Glenn <glennsay1234@gmail.com>
2024-08-27 23:01:21 +0800

    Update help message to reflect new features

--- src/main/java/Ginger.java
+++ src/main/java/Ginger.java
@@ -20,7 +20,10 @@ public class Ginger {
             "todo - Adds a To Do task. Usage: todo <title>\n" +
             "deadline - Adds a Deadline task which includes a deadline. Usage: deadline <title> /by <time>\n" +
             "event - Adds an Event task which has start and end time. Usage: event <title> /from <time> /to <time>\n" +
-            "help - Displays this set of information again.";
+            "help - Displays this set of information again.\n" +
+            "delete - Deletes a task from the list. Usage: delete <task number>\n\n" +
+            "Do note that Ginger supports date and time formatting. Please follow the convention dd/mm/yyyy HHmm\n" +
+            "when entering your time! Examples: 01/08/2024 1830, 1/8/2024 1830 is also accepted.";
     private final static ArrayList<Task> taskList = new ArrayList<>();
 
     enum Command {
