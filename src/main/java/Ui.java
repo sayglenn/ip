64ec458b98cd039b40dadc5e502b9afe10bcebcc
Say Glenn <glennsay1234@gmail.com>
2024-08-28 11:27:03 +0800

    Add Ui class to abstract user interface handling

---
+++ src/main/java/Ui.java
@@ -0,0 +1,14 @@
+public class Ui {
+    private final String HORIZONTAL_LINE = "____________________________________________________________";
+    private final String BOT_NAME = "Ginger";
+
+    public Ui() { }
+
+    public void outputMessage(String message) {
+        System.out.println(HORIZONTAL_LINE + "\n" + message + "\n" + HORIZONTAL_LINE);
+    }
+
+    public void welcomeMessage() {
+        outputMessage(String.format("Hello! I'm %s\nHow can I spice things up for you?", BOT_NAME));
+    }
+}
